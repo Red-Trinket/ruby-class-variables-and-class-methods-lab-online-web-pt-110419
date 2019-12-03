4e713e51cfe97529e338e2528604b597fc2ee698
Matthew Jones <matts3371@gmail.com>
2019-12-03 19:46:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,22 @@
-class Song 
+
+  class Song 
+  @@count = 0
   
+  def self.count 
+    @@count
+  end 
   
+  # Control for duplicates when you code your 
+  # .genres class method, not when you add 
+  # genres to the original @@genres array.
+  @@genres = []
   
-end
+# this should be deleted at some point
+# def self.total_genre_entries
+#   @@genres
+# end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
